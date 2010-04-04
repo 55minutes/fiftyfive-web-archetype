28fc0fd387712f9c612e4003f4165ca2f60c891c
Matt Brictson <matt@55minutes.com>
2010-04-04 22:18:04 +0000

    Add a script for testing the archetype in hudson.
    git-svn-id: http://opensource.55minutes.com/svn/java/trunk/fiftyfive-web-archetype@284 3a26569e-5468-0410-bb2f-b495330926e5

---
+++ ci-test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# This script is for continuous integration. Generates a test project from
+# the archetype and then runs `mvn test` on it to make sure everything works.
+
+mkdir temp
+cd temp
+mvn archetype:generate -B -U \
+    -DarchetypeGroupId=com.55minutes \
+    -DarchetypeArtifactId=fiftyfive-web-archetype \
+    -DarchetypeRepository=http://opensource.55minutes.com/maven \
+    -DarchetypeVersion=1.0-SNAPSHOT \
+    -DgroupId=com.55minutes \
+    -DartifactId=test-project \
+    -Dversion=999 \
+    -Dpackage=fiftyfive.test \
+    -Dproject_name=Test && \
+cd test-project && \
+mvn -B test
